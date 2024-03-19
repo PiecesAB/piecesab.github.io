5c9f077ce4bb42b944a51727c41d68ba75af0046
Questionrat <140145679+PiecesAB@users.noreply.github.com>
2024-03-19 16:17:10 -0700

    different title and subtitle

--- load-common.js
+++ load-common.js
@@ -1,4 +1,4 @@
-window.onload = function LoadCommon() {
+function AddNavigator() {
 	window.document.body.insertAdjacentHTML("afterbegin", 
 	`
 	<div id="navigator">
@@ -7,20 +7,44 @@ window.onload = function LoadCommon() {
 		<button class = "nav-button" onclick="window.location.href='/hellsyncer/index.html'">HellSyncer</button>
 	</div>
 	`);
+}
+
+function AddHeader() {
+	let title = "Pieces-AB";
+	let subtitle = "Questionrat's Evil Lab";
+	if (window.location.pathname.includes("blastula")) {
+		title = "Blastula";
+		subtitle = "It's &quot;easy&quot;!"
+	}
+	else if (window.location.pathname.includes("hellsyncer")) {
+		title = "HellSyncer";
+		subtitle = "Now reaches the fatal attraction be described by music synchronization"
+	}
+	
 	window.document.body.insertAdjacentHTML("afterbegin", 
 	`
 	<div id="header">
 		<div class="primary-container">
-			<span class="title">Pieces-AB</span>
+			<span class="title">${title}</span>
 			<hr />
-			<span class="subtitle">Questionrat's Evil Lab</span>
+			<span class="subtitle">${subtitle}</span>
 		</div>
 	</div>
 	`);
+}
+
+function AddFooter() {
+	// Add footer
 	window.document.body.insertAdjacentHTML("beforeend", 
 	`
 	<div id="footer">
 		&mdash;This is a personal website, not to be copied without permission. &copy; 2024 "Questionrat"&mdash;
 	</div>
 	`);
+}
+
+window.onload = function LoadCommon() {
+	AddNavigator();
+	AddHeader();
+	AddFooter();
 }
